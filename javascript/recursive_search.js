2fe1907878e79e1a5282acd751913fa26994974a
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-29 12:59:41 -0600

    complete

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,12 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  console.log(arr);
+  if (arr.length === 0) {
+    return false;
+  } else if (arr[0] === target) {
+    return true;
+  } else {
+    return recursiveSearch(arr.slice(1), target);
+  }
 }
 
 if (require.main === module) {
@@ -17,3 +24,10 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * function recursiveSearch(arr, target) {
+ * if the array is empty return false
+ * else if the first index in the array is target, return true
+ * else recursiveSearch(arr.slice(1, target))
+ * }
+ */
